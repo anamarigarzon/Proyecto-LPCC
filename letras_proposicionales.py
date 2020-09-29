2ab5e8fa422ad1c7b83e267796d00ad083b81069
anamarigarzon <63021584+anamarigarzon@users.noreply.github.com>
2020-09-29 17:51:38 -0500

    Update letras_proposicionales.py

--- letras_proposicionales.py
+++ letras_proposicionales.py
@@ -1,11 +1,14 @@
 #letras_proposicionales
+#Creación, codificación y decodificación
 
-
-def num_letras(Nd,Np,Nc,Ng,Ntd): #Función que saca el número de letras proposicionales del problema
+def num_letras(Nd,Np,Nc,Ng,Ntd): #Recibe como argumentos la cantidad de días, prisioneros, colores, guardias y tipos de días
+    #Función que saca el número de letras proposicionales del problema
     
     return Nd*Np*Nc*Ng*Ntd
+
     
 def codifica(d, p, c, g, td, Nd, Np, Nc, Ng, Ntd): #Recibe como argumentos el dia, prisionero, color, guardia, tipo de día, y la respectiva cantidad de cada uno de estos atributos para el problema
+    #codifica el valor de una letra proposicional a partir de sus atributos
     
     assert((d >= 0) and (d <= Nd - 1)), 'Primer argumento incorrecto! Debe ser un numero entre 0 y ' + str(Nd - 1)  + "\nSe recibio " + str(d)
     assert((p >= 0) and (p <= Np - 1)), 'Segundo argumento incorrecto! Debe ser un numero entre 0 y ' + str(Np - 1)  + "\nSe recibio " + str(p)
@@ -13,20 +16,22 @@ def codifica(d, p, c, g, td, Nd, Np, Nc, Ng, Ntd): #Recibe como argumentos el di
     assert((g >= 0) and (g <= Ng - 1)), 'Cuarto argumento incorrecto! Debe ser un numero entre 0 y ' + str(Ng - 1)  + "\nSe recibio " + str(g)
     assert((td >= 0) and (td <= Ntd - 1)), 'Quinto argumento incorrecto! Debe ser un numero entre 0 y ' + str(Ntd - 1)  + "\nSe recibio " + str(td)
     
-    p = p+12
+    #Modificar los valores con el fin de que al operarlos con la ecuación de abajo produzcan un entero único
+    
+    p = p+12 
     c = c+17
     g = g+19
     td = td+21
         
-    n = td*p*c*g + d
+    n = td*p*c*g + d #Entero único para este problema
     
-    return n
+    return n #retorna variable que contiene al entero único
 
-def lista_letras(Nd,Np,Nc,Ng,Ntd): #retorna una lista con todos los valores de las letras proposicionales
+
+def lista_val_letras(Nd,Np,Nc,Ng,Ntd): #Recibe como argumentos la cantidad de días, prisioneros, colores, guardias y tipos de días
+    #genera una lista con cada uno de los valores de las letras proposicionales
     
-    n=0
-
-    mylist = []
+    mylist = [] # lista vacía
 
     for d in range(Nd):
         for p in range(Np):
@@ -34,13 +39,22 @@ def lista_letras(Nd,Np,Nc,Ng,Ntd): #retorna una lista con todos los valores de l
                 for g in range(Ng):
                     for t in range(Ntd):
                         v1 = codifica(d, p, c, g, t, Nd, Np, Nc, Ng, Ntd)
-                        print(v1, end=" ")
-                        mylist.append(v1)
-                        n = n+1
-    assert()
-    return mylist
+                        mylist.append(v1) #Se guarda cada uno de los valores de las letras proposicionales en mylist
+                        
+    return mylist #Retorna lista con todos los valores de cada una las letras proposicionales
 
-def prueba_codifica(n, mylist): #Prueba que, efectivamente, codifica funciona y todos los valores de las letras proposicionales son distintos
+
+def lista_letras(n): #Recibe el número de letras proposicionales, y retorna una lista con n caracteres distintos
+    letras = []
+    
+    for i in range(n):
+        letras.append(chr(i+200))
+    
+    return letras        
+
+
+def prueba_codifica(n, mylist): #Recibe como argumentos el número de letras proposicionales, y una lista que contiene los valores de las letras proposicionales
+    #Función que prueba si en efecto, el valor de cada una de las letras proposicionales es único
     
     j = 0
 
@@ -53,4 +67,48 @@ def prueba_codifica(n, mylist): #Prueba que, efectivamente, codifica funciona y 
             j = j+1
     
     if(j==n):
+        
         print("Los números no se repiten, perfecto")
+        
+
+def decodifica(n, k, lista1, Nd, Np, Nc, Ng, Ntd): #Recibe como atributos la letra proposicional n, el número de letras proposicionales k, una lista con los valores de las letras proposicionales, y la cantidad de cada uno de los atributos mencionados anteriormente
+    #Función que decodifica una letra proposicional, y retorna una lista con sus atributos, ordenados en día, prisionero,color, guardia y tipo de día
+    
+    n = ord(n)
+    
+    assert((n >= 200) and (n <= k+200)), 'Codigo incorrecto! Debe estar entre 200 y' + str(k+200) + "\nSe recibio " + str(n)
+    
+    j = int(n) - 200 # restarle 200 al número para obtener la 
+    
+    num = lista1[j]
+
+    for d in range(Nd):
+        for p in range(Np):
+            for c in range(Nc):
+                for g in range(Ng):
+                    for t in range(Ntd):
+                        v1 = codifica(d, p, c, g, t, Nd, Np, Nc, Ng, Ntd)
+                        if v1 == num:
+                            atributos = [d,p,c,g,t] #lista con sus atributos, ordenados en día, prisionero,color, guardia y tipo de día, respectivamente
+                            break
+    
+    return atributos
+   
+    
+    
+#BEGINNING-OF-EXECUTION
+    
+n = num_letras(12,5,2,2,2)
+lista = lista_val_letras(12,5,2,2,2)
+
+prueba_codifica(n, lista)
+
+g = chr(200)
+a = decodifica(g,480,lista,12,5,2,2,2)
+
+print ("Letra: ",g)
+print ("Código: [ ", end="")
+for i in a:
+    print (i,end= ", ")
+print ("]")
+#END-OF-EXECUTION
