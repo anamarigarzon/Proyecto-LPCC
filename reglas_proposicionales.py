39b6a153e590ccd5a036f4234361ccb90a7d037e
anamarigarzon <63021584+anamarigarzon@users.noreply.github.com>
2020-09-29 19:08:22 -0500

    Update reglas_proposicionales.py

--- reglas_proposicionales.py
+++ reglas_proposicionales.py
@@ -1,1 +1,9 @@
-#reglas proposicionales
+#Reglas Proposicionales
+
+#Color, si el atributo es cero, es rojo y si es uno, es amarillo.
+#Guardia, si el atributo es cero, es de visión normal y si es uno, es daltónico
+#Tipo de día, si el atributo es cero, es par y si es uno, es impar
+#Día, corresponde al atributo + 1
+#Prisionero, corresponde al atributo + 1
+
+import letras_proposicionales
