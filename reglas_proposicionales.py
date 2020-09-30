c6998acba3be39ec5be9cfd3a67ba0ba2a074a53
anamarigarzon <63021584+anamarigarzon@users.noreply.github.com>
2020-09-29 21:56:27 -0500

    Update reglas_proposicionales.py

--- reglas_proposicionales.py
+++ reglas_proposicionales.py
@@ -1,10 +1,4 @@
-#Reglas Proposicionales
 
-#Color, si el atributo es cero, es rojo y si es uno, es amarillo.
-#Guardia, si el atributo es cero, es de visión normal y si es uno, es daltónico
-#Tipo de día, si el atributo es cero, es par y si es uno, es impar
-#Día, corresponde al atributo + 1
-#Prisionero, corresponde al atributo + 1
 
 import letras_proposicionales
 
@@ -14,19 +8,23 @@ import letras_proposicionales
 letras = letras_proposicionales.lista_letras(480)
 lista = letras_proposicionales.lista_val_letras(12,5,2,2,2)
 
-rojos = []
-amarillos=[]
+prisioneros = [0,1,2,3,4]
+form_regla1 = ""
+temp = ""
 
-for i in letras:
-    h=letras_proposicionales.decodifica(i,480,lista,12,5,2,2,2)
-    if (h[2]==0):
-        rojos.append(h)
-    else:
-        amarillos.append(h)
+for j in prisioneros:
+    form_regla1 += "("
+    for i in letras:
+        h=letras_proposicionales.decodifica(i,480,lista,12,5,2,2,2)
+        if h[1] == j:            
+            if (h[2]==0):
+                form_regla1 += str(i)
+                form_regla1 += "O"
+            
+    regla1= regla1 + ")"
         
 regla1 = ""
         
 #Tomar todas las letras proposicionales que tengan color rojo, y unirlas todas con un O 
 #Luego, lo anterior implica (-->)
 #Negar todas las letras proposicionales que tengan amarillo, y unirlas todas con un Y
-#Concatenar todo en una fórmula en el string regla 1
