b5a690589cf559d8e041b84a555377769d62572d
anamarigarzon <63021584+anamarigarzon@users.noreply.github.com>
2020-10-19 16:51:31 -0500

    Update reglas_proposicionales.py

--- reglas_proposicionales.py
+++ reglas_proposicionales.py
@@ -1,29 +1,19 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Oct 12 18:01:03 2020
 
+@author: Ana Garzón y Gabriela Linares
 
-import letras_proposicionales
+Reglas en Lógica Proposicional
 
+Proyecto Lógica para Ciencias de la Computación
+"""
+import codificacion_letras
 
-#Regla 1: Un prisionero o es rojo, o es amarillo, pero no ambas
+#Regla 1:
 
-letras = letras_proposicionales.lista_letras(480)
-lista = letras_proposicionales.lista_val_letras(12,5,2,2,2)
-
-prisioneros = [0,1,2,3,4]
-form_regla1 = ""
-temp = ""
-
-for j in prisioneros:
-    form_regla1 += "("
-    for i in letras:
-        h=letras_proposicionales.decodifica(i,480,lista,12,5,2,2,2)
-        if h[1] == j:            
-            if (h[2]==0):
-                form_regla1 += str(i)
-                form_regla1 += "O"
-            
-    form_regla1= form_regla1 + ")"
-        
-        
-#Tomar todas las letras proposicionales que tengan color rojo, y unirlas todas con un O 
-#Luego, lo anterior implica (-->)
-#Negar todas las letras proposicionales que tengan amarillo, y unirlas todas con un Y
+#Regla 2:
+    
+#Regla 3:
+    
+#Regla 4:
