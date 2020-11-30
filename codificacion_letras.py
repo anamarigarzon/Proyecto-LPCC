7b7116f568852e9346550531a94b31983632b96a
anamarigarzon <63021584+anamarigarzon@users.noreply.github.com>
2020-11-29 21:13:07 -0500

    Update codificacion_letras.py

--- codificacion_letras.py
+++ codificacion_letras.py
@@ -5,6 +5,7 @@ Created on Tue Sep 29 08:11:58 2020
 Codificación Letras Proposicionales
 Proyecto Lógica para Ciencias de la Computación
 """
+print("")
 
 print("-------CODIFICACIÓN LETRAS PROPOSICIONALES---------")
 
@@ -22,39 +23,6 @@ def decodifica(n, Nf, Nc):
     f = int(n / Nc)
     c = n % Nc
     return f, c
-
-Nfilas = 5
-Ncolumnas = 2
-
-print(u"Números correspondientes a la codificación")
-print("\nfilas x columnas")
-for i in range(Nfilas):
-    for j in range(Ncolumnas):
-        v1 = codifica(i, j, Nfilas, Ncolumnas)
-        print(v1, end = " ")
-    print("")
-print("\n")
-
-for v1 in range(10):
-    f, c = decodifica(v1, Nfilas, Ncolumnas)
-    print('Código: '+str(v1)+', Fila: '+str(f)+', Columna: '+str(c))
-
-letras = []
-print("\n\nfilas x columnas")
-for i in range(Nfilas):
-    for j in range(Ncolumnas):
-        v1 = codifica(i, j, Nfilas, Ncolumnas)
-        cod = chr(v1 + 256)
-        print(cod, end = " ")
-        letras.append(cod)
-    print("")
-print("\n")
-
-for cod in letras:
-    print('Letra = '+cod, end=', ')
-    f, c = decodifica(ord(cod)-256, Nfilas, Ncolumnas)
-    print('Fila = '+str(f), end=', ')
-    print('Columna = '+str(c))
     
 def P(f, c, o, Nf, Nc, No):
     # Funcion que codifica tres argumentos
@@ -73,30 +41,63 @@ def Pinv(codigo, Nf, Nc, No):
     f, c = decodifica(v1, Nf, Nc)
     return f, c, o
 
+Nfilas = 5
+Ncolumnas = 2
+
+'''
+print(u"Números correspondientes a la codificación")
+print("\nfilas x columnas")
+for i in range(Nfilas):
+    for j in range(Ncolumnas):
+        v1 = codifica(i, j, Nfilas, Ncolumnas)
+        print(v1, end = " ")
+    print("")
 print("\n")
+
+for v1 in range(10):
+    f, c = decodifica(v1, Nfilas, Ncolumnas)
+    print('Código: '+str(v1)+', Fila: '+str(f)+', Columna: '+str(c))
+    
+
+
+letras = []
+#print("\n\nfilas x columnas")
+for i in range(Nfilas):
+    for j in range(Ncolumnas):
+        v1 = codifica(i, j, Nfilas, Ncolumnas)
+        cod = chr(v1 + 256)
+        #print(cod, end = " ")
+        letras.append(cod)
+    #print("")
+print("\n")
+'''
+
+
 letras = []
 n = 0
 Nnumeros = 5
 for k in range(Nnumeros):
-    print("Prisionero: "+str(k))
-    print("filas x columnas")
+    #print("Prisionero: "+str(k))
+    #print("filas x columnas")
     for i in range(Nfilas):
         for j in range(Ncolumnas):
             n += 1
             cod = P(i, j, k, Nfilas, Ncolumnas, Nnumeros)
-            print(cod, end = " ")
+            #print(cod, end = " ")
             letras.append(cod)
-        print("")
-    print('\n')
+        #print("")
+    #print('\n')
     
 
-
+print("")
 print("Total de letras proposicionales: ", n)
+print("")
 
 for cod in letras:
     print('Letra = '+cod, end=', ')
     f, c, o = Pinv(cod, Nfilas, Ncolumnas, Nnumeros)
     print('Prisionero = '+str(o), end=', ')
     print('Fila = '+str(f), end=', ')
-    print('Columna = '+str(c), end=',')
-    print('Número de letra: ', ord(cod))
+    print('Columna = '+str(c) )
+
+print("")
